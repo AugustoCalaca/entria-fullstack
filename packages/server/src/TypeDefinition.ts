de10472315ca21d4a7391f700cfb4f80a77df4e7
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-20 23:47:30 -0300

    add IBook on context and BookLoader on Dataloaders

--- packages/server/src/TypeDefinition.ts
+++ packages/server/src/TypeDefinition.ts
@@ -1,14 +1,17 @@
 import Dataloader from 'dataloader';
 
 import { IUser } from './modules/user/UserModel';
+import { IBook } from './modules/book/BookModel';
 
 type Key = string;
 
 export type Dataloaders = {
   UserLoader: Dataloader<Key, IUser>;
+  BookLoader: Dataloader<Key, IBook>;
 };
 
 export type GraphQLContext = {
   user?: IUser;
+  book?: IBook;
   dataloaders: Dataloaders;
 };
