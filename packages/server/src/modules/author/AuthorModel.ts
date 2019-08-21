f7551bf9449684a8ddecb01a04e5b503ffc719ec
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-20 23:51:40 -0300

    add AuthorModel

---
+++ packages/server/src/modules/author/AuthorModel.ts
@@ -0,0 +1,24 @@
+import mongoose, { Model, Document } from 'mongoose';
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+}, {
+  timestamps: true
+});
+
+export interface IAuthor extends Document {
+  name: String,
+  age: Number
+}
+
+const AuthorModel: Model<IAuthor> = mongoose.model('Author', schema);
+
+export default AuthorModel;
