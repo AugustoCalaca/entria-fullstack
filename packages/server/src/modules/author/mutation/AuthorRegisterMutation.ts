8bdc9f57fa4224ea59fe0d5ca604b4a27561ea1b
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-21 23:50:05 -0300

    add AuthorRegister

---
+++ packages/server/src/modules/author/mutation/AuthorRegisterMutation.ts
@@ -0,0 +1,50 @@
+import { GraphQLString, GraphQLInt, GraphQLNonNull } from 'graphql'
+import { mutationWithClientMutationId } from 'graphql-relay';
+
+import AuthorModel from '../AuthorModel';
+import pubSub, { EVENTS } from '../../../pubSub';
+
+export default mutationWithClientMutationId({
+  name: 'AuthorRegister',
+  inputFields: {
+    name: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    age: {
+      type: new GraphQLNonNull(GraphQLInt)
+    }
+  },
+  mutateAndGetPayload: async({ name, age }) => {
+    let author = await AuthorModel.findOne({ name, age });
+
+    if(author) {
+      return {
+        error: 'Author already exists'
+      };
+    }
+
+    author = new AuthorModel({ name, age });
+    author.save();
+
+    await pubSub.publish(EVENTS.AUTHOR.ADDED, { AuthorAdded: { author } })
+
+    return {
+      name: author.name,
+      age: author.age
+    };
+  },
+  outputFields: {
+    name: {
+      type: GraphQLString,
+      resolve: ({ name }) => name
+    },
+    age: {
+      type: GraphQLInt,
+      resolve: ({ age }) => age
+    },
+    error: {
+      type: GraphQLString,
+      resolve: ({ error }) => error
+    }
+  }
+});
