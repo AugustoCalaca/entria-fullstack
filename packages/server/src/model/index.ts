26cdf748833693c89094c4db53684dcd0be867bd
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-21 23:47:38 -0300

    export Author and Book of the models

--- packages/server/src/model/index.ts
+++ packages/server/src/model/index.ts
@@ -1,1 +1,3 @@
 export { default as User } from '../modules/user/UserModel';
+export { default as Author } from '../modules/author/AuthorModel';
+export { default as Book } from '../modules/book/BookModel';
