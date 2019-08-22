83aea1d6c1dfde8e52bd8305a301fb3bfa4e2691
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-21 23:45:30 -0300

    add events to book added and author added

--- packages/server/src/pubSub.ts
+++ packages/server/src/pubSub.ts
@@ -4,6 +4,12 @@ export const EVENTS = {
   USER: {
     ADDED: 'USER_ADDED',
   },
+  AUTHOR: {
+    ADDED: 'AUTHOR_ADDED',
+  },
+  BOOK: {
+    ADDED: 'BOOK_ADDED',
+  }
 };
 
 export default new PubSub();
