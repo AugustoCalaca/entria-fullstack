54ff07e4daeb86b84678607cf9727f1435667961
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-21 23:44:43 -0300

    add AuthorLoader on dataloaders and IAuthor on context

--- packages/server/src/TypeDefinition.ts
+++ packages/server/src/TypeDefinition.ts
@@ -1,17 +1,20 @@
 import Dataloader from 'dataloader';
 
 import { IUser } from './modules/user/UserModel';
+import { IAuthor } from './modules/author/AuthorModel';
 import { IBook } from './modules/book/BookModel';
 
 type Key = string;
 
 export type Dataloaders = {
   UserLoader: Dataloader<Key, IUser>;
+  AuthorLoader: Dataloader<Key, IAuthor>;
   BookLoader: Dataloader<Key, IBook>;
 };
 
 export type GraphQLContext = {
   user?: IUser;
+  author?: IAuthor;
   book?: IBook;
   dataloaders: Dataloaders;
 };
