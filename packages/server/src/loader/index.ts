524a9c1e83ea9d0b704362f7b553369247b7b1c0
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-21 23:46:49 -0300

    add AuthorLoader and BookLoader in main loader

--- packages/server/src/loader/index.ts
+++ packages/server/src/loader/index.ts
@@ -1,3 +1,9 @@
 import * as UserLoader from '../modules/user/UserLoader';
+import * as AuthorLoader from '../modules/author/AuthorLoader';
+import * as BookLoader from '../modules/book/BookLoader';
 
-export { UserLoader };
+export {
+  UserLoader,
+  AuthorLoader,
+  BookLoader
+};
