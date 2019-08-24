1b50830300133a558e2535211904d970ec320bd6
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-24 17:08:09 -0300

    add tests to book and author node

--- packages/server/src/interface/__tests__/NodeInterface.spec.ts
+++ packages/server/src/interface/__tests__/NodeInterface.spec.ts
@@ -41,3 +41,60 @@ it('should load User', async () => {
   expect(result.data.node.id).toBe(variables.id);
   expect(result.data.node.name).toBe(user.name);
 });
+
+it('should load Book', async () => {
+  const book = await createRows.createBook();
+
+  // language=GraphQL
+  const query = `
+    query Q($id: ID!) {
+      node(id: $id) {
+        id
+        ... on Book {
+          title
+        }
+      }
+    }
+  `;
+
+  const rootValue = {};
+  const context = getContext();
+  const variables = {
+    id: toGlobalId('Book', book.id),
+  };
+
+  const result = await graphql(schema, query, rootValue, context, variables);
+  const { node } = result.data;
+
+  expect(node.id).toBe(variables.id);
+  expect(node.title).toBe(book.title);
+
+});
+
+it('should load Author', async () => {
+  const author = await createRows.createAuthor();
+
+  // language=GraphQL
+  const query = `
+    query Q($id: ID!) {
+      node(id: $id) {
+        id
+        ... on Author {
+          name
+        }
+      }
+    }
+  `;
+
+  const rootValue = {};
+  const context = getContext();
+  const variables = {
+    id: toGlobalId('Author', author.id),
+  };
+
+  const result = await graphql(schema, query, rootValue, context, variables);
+  const { node } = result.data;
+
+  expect(node.id).toBe(variables.id);
+  expect(node.name).toBe(author.name);
+});
