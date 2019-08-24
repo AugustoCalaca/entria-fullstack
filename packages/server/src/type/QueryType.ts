3eb681802710f816a5f324b5d9215fc65f719c2b
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-23 23:58:19 -0300

    add queries to authors and books

--- packages/server/src/type/QueryType.ts
+++ packages/server/src/type/QueryType.ts
@@ -4,8 +4,11 @@ import { GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLID } from 'gra
 import { connectionArgs, fromGlobalId } from 'graphql-relay';
 
 import UserType, { UserConnection } from '../modules/user/UserType';
+import AuthorType, { AuthorConnection } from '../modules/author/AuthorType';
+import BookType, { BookConnection } from '../modules/book/BookType';
+
 import { nodeField } from '../interface/NodeInterface';
-import { UserLoader } from '../loader';
+import { UserLoader, AuthorLoader, BookLoader } from '../loader';
 
 export default new GraphQLObjectType({
   name: 'Query',
@@ -37,6 +40,50 @@ export default new GraphQLObjectType({
         },
       },
       resolve: (obj, args, context) => UserLoader.loadUsers(context, args),
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID),
+        },
+      },
+      resolve: (obj, args, context) => {
+        const { id } = fromGlobalId(args.id);
+        return AuthorLoader.load(context, id);
+      },
+    },
+    authors: {
+      type: AuthorConnection.connectionType,
+      args: {
+        ...connectionArgs,
+        search: {
+          type: GraphQLString,
+        },
+      },
+      resolve: (obj, args, context) => AuthorLoader.loadAuthors(context, args),
+    },
+    book: {
+      type: BookType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID),
+        },
+      },
+      resolve: async (obj, args, context) => {
+        const { id } = fromGlobalId(args.id);
+        return BookLoader.load(context, id);
+      },
+    },
+    books: {
+      type: BookConnection.connectionType,
+      args: {
+        ...connectionArgs,
+        search: {
+          type: GraphQLString,
+        },
+      },
+      resolve: (obj, args, context) => BookLoader.loadBooks(context, args),
     },
   }),
 });
