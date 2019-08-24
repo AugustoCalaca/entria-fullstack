f363200be033bac09b7862cbd8a10da519f81695
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-23 23:51:28 -0300

    resolve book connections

--- packages/server/src/modules/author/AuthorType.ts
+++ packages/server/src/modules/author/AuthorType.ts
@@ -1,8 +1,11 @@
 import { GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql';
-import { globalIdField } from 'graphql-relay';
+import { globalIdField, connectionArgs } from 'graphql-relay';
 
 import { connectionDefinitions } from '../../core/connection/CustomConnectionType';
 import { registerType, nodeInterface } from '../../interface/NodeInterface';
+
+import { BookConnection } from '../book/BookType';
+import { BookLoader } from '../../loader';
 
 const AuthorType = registerType(
   new GraphQLObjectType({
@@ -21,6 +24,20 @@ const AuthorType = registerType(
       age: {
         type: GraphQLInt,
         resolve: author => author.age
+      },
+      books: {
+        type: BookConnection.connectionType,
+        args: {
+          ...connectionArgs,
+          search: {
+            type: GraphQLString,
+          },
+        },
+        resolve: (author, args, context) => {
+          const findBooksOfAuthor = author.books;
+          args = { ...args, findBooksOfAuthor }
+          return BookLoader.loadBooks(context, args);
+        }
       }
     }),
     interfaces: () => [nodeInterface]
