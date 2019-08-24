7f3de2e2d923efb8007d79b7d61e82d7113a307e
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-23 23:57:11 -0300

    add AuthorMutations and BookMutations

--- packages/server/src/type/MutationType.ts
+++ packages/server/src/type/MutationType.ts
@@ -3,10 +3,14 @@
 import { GraphQLObjectType } from 'graphql';
 
 import UserMutations from '../modules/user/mutation';
+import AuthorMutations from '../modules/author/mutation';
+import BookMutations from '../modules/book/mutation';
 
 export default new GraphQLObjectType({
   name: 'Mutation',
   fields: () => ({
     ...UserMutations,
+    ...AuthorMutations,
+    ...BookMutations
   }),
 });
