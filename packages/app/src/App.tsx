f1310403b1f9cbd519bce7ac7019e3ffb6d8f5ee
AugustoCalaca <augustocalaca2@gmail.com>
2019-09-06 02:33:00 -0300

    add state logged to controller component route

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'react-native';
 import { LoggedOutApp, LoggedInApp } from './Routes';
-import { getToken } from './auth';
+import { getToken } from './relay/helpers';
 
 const App = () => {
-  const logged = getToken();
-  console.log('logged');
-  console.log(logged);
+  const [logged, setLogged] = useState<boolean>(false);
+
+  async function loadToken() {
+    try {
+      const token = await getToken();
+      if(token.substr(0, 3) === 'JWT')
+      setLogged(true);
+		} catch(err) {
+      setLogged(false);
+		}
+  };
+
+  useEffect(() => {
+    loadToken();
+  }, []);
 
   return (
     <>
-      <StatusBar backgroundColor="#271E85" barStyle="light-content"s />
-      {logged ? <LoggedInApp /> : <LoggedOutApp />}
+      <StatusBar backgroundColor="#271E85" barStyle="light-content" />
+      {logged
+        ? <LoggedInApp screenProps={{setLogged}} />
+        : <LoggedOutApp screenProps={{setLogged}} />
+      }
     </>
   )
 };
